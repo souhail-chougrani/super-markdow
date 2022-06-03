dae4a94a0904fe01462d7bf9c2dafd8d9024efdc
Souhail <souhail.chougrani@sii-maron.com>
2022-06-03 19:15:33 +0200

    add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     SharedModule,
     ReactiveFormsModule,
+    StoreModule.forRoot({}, {}),
 
   ],
   providers: [],
